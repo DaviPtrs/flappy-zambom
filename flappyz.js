b6a11a9c6a722f376edc3ceb3817acf7de26dd25
vfmb <vfmbrunoro@gmail.com>
2021-04-02 15:30:36 -0300

    base

---
+++ flappyz.js
@@ -0,0 +1,370 @@
+
+// SELECT board
+const board = document.getElementById ("zamboard");
+const boardContext = board.getContext ("2d");
+
+
+// GAME VARS AND CONSTS
+let frames = 0;
+const DEGREE = Math.PI/180;
+
+// LOAD SPRITE IMAGE
+const sprite = new Image();
+sprite.src = "img/sprite.png";
+
+// LOAD SOUNDS
+const SCORE_S = new Audio();
+SCORE_S.src = "audio/sfx_point.wav";
+
+const FLAP = new Audio();
+FLAP.src = "audio/sfx_flap.wav";
+
+const HIT = new Audio();
+HIT.src = "audio/sfx_hit.wav";
+
+const SWOOSHING = new Audio();
+SWOOSHING.src = "audio/sfx_swooshing.wav";
+
+const DIE = new Audio();
+DIE.src = "audio/sfx_die.wav";
+
+// GAME STATE
+const state = {
+    current : 0,
+    getReady : 0,
+    game : 1,
+    over : 2
+}
+
+// START BUTTON COORD
+const startBtn = {
+    x : 120,
+    y : 263,
+    w : 83,
+    h : 29
+}
+
+// CONTROL THE GAME
+board.addEventListener("click", function(event){
+    switch(state.current){
+        case state.getReady:
+            state.current = state.game;
+            SWOOSHING.play();
+            break;
+        case state.game:
+            if(zambird.y - zambird.radius <= 0) return;
+            zambird.flap();
+            FLAP.play();
+            break;
+        case state.over:
+            let rect = board.getBoundingClientRect();
+            let clickX = event.clientX - rect.left;
+            let clickY = event.clientY - rect.top;
+            
+            // CHECK IF WE CLICK ON THE START BUTTON
+            if(clickX >= startBtn.x && clickX <= startBtn.x + startBtn.w && clickY >= startBtn.y && clickY <= startBtn.y + startBtn.h){
+                pipes.reset();
+                zambird.speedReset();
+                score.reset();
+                state.current = state.getReady;
+            }
+            break;
+    }
+});
+
+
+// BACKGROUND
+const background = {
+    sX : 0,
+    sY : 0,
+    w : 275,
+    h : 226,
+    x : 0,
+    y : board.height - 226,
+    
+    draw : function(){
+        boardContext.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        
+        boardContext.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+    
+}
+
+// FOREGROUND
+const foreground = {
+    sX: 276,
+    sY: 0,
+    w: 224,
+    h: 112,
+    x: 0,
+    y: board.height - 112,
+    
+    dx : 2,
+    
+    draw : function(){
+        boardContext.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        
+        boardContext.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    },
+    
+    update: function(){
+        if(state.current == state.game){
+            this.x = (this.x - this.dx)%(this.w/2);
+        }
+    }
+}
+
+// BIRD
+const zambird = {
+    animation : [
+        {sX: 276, sY : 112},
+        {sX: 276, sY : 139},
+        {sX: 276, sY : 164},
+        {sX: 276, sY : 139}
+    ],
+    x : 50,
+    y : 150,
+    w : 34,
+    h : 26,
+    
+    radius : 12,
+    
+    frame : 0,
+    
+    gravity : 0.25,
+    jump : 4.6,
+    speed : 0,
+    rotation : 0,
+    
+    draw : function(){
+        let zambird = this.animation[this.frame];
+        
+        boardContext.save();
+        boardContext.translate(this.x, this.y);
+        boardContext.rotate(this.rotation);
+        boardContext.drawImage(sprite, zambird.sX, zambird.sY, this.w, this.h,- this.w/2, - this.h/2, this.w, this.h);
+        
+        boardContext.restore();
+    },
+    
+    flap : function(){
+        this.speed = - this.jump;
+    },
+    
+    update: function(){
+        // IF THE GAME STATE IS GET READY STATE, THE BIRD MUST FLAP SLOWLY
+        this.period = state.current == state.getReady ? 10 : 5;
+        // WE INCREMENT THE FRAME BY 1, EACH PERIOD
+        this.frame += frames%this.period == 0 ? 1 : 0;
+        // FRAME GOES FROM 0 To 4, THEN AGAIN TO 0
+        this.frame = this.frame%this.animation.length;
+        
+        if(state.current == state.getReady){
+            this.y = 150; // RESET POSITION OF THE BIRD AFTER GAME OVER
+            this.rotation = 0 * DEGREE;
+        }else{
+            this.speed += this.gravity;
+            this.y += this.speed;
+            
+            if(this.y + this.h/2 >= board.height - foreground.h){
+                this.y = board.height - foreground.h - this.h/2;
+                if(state.current == state.game){
+                    state.current = state.over;
+                    DIE.play();
+                }
+            }
+            
+            // IF THE SPEED IS GREATER THAN THE JUMP MEANS THE BIRD IS FALLING DOWN
+            if(this.speed >= this.jump){
+                this.rotation = 90 * DEGREE;
+                this.frame = 1;
+            }else{
+                this.rotation = -25 * DEGREE;
+            }
+        }
+        
+    },
+    speedReset : function(){
+        this.speed = 0;
+    }
+}
+
+// GET READY MESSAGE
+const getReady = {
+    sX : 0,
+    sY : 228,
+    w : 173,
+    h : 152,
+    x : board.width/2 - 173/2,
+    y : 80,
+    
+    draw: function(){
+        if(state.current == state.getReady){
+            boardContext.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        }
+    }
+    
+}
+
+// GAME OVER MESSAGE
+const gameOver = {
+    sX : 175,
+    sY : 228,
+    w : 225,
+    h : 202,
+    x : board.width/2 - 225/2,
+    y : 90,
+    
+    draw: function(){
+        if(state.current == state.over){
+            boardContext.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);   
+        }
+    }
+    
+}
+
+// PIPES
+const pipes = {
+    position : [],
+    
+    top : {
+        sX : 553,
+        sY : 0
+    },
+    bottom:{
+        sX : 502,
+        sY : 0
+    },
+    
+    w : 53,
+    h : 400,
+    gap : 85,
+    maxYPos : -150,
+    dx : 2,
+    
+    draw : function(){
+        for(let i  = 0; i < this.position.length; i++){
+            let p = this.position[i];
+            
+            let topYPos = p.y;
+            let bottomYPos = p.y + this.h + this.gap;
+            
+            // top pipe
+            boardContext.drawImage(sprite, this.top.sX, this.top.sY, this.w, this.h, p.x, topYPos, this.w, this.h);  
+            
+            // bottom pipe
+            boardContext.drawImage(sprite, this.bottom.sX, this.bottom.sY, this.w, this.h, p.x, bottomYPos, this.w, this.h);  
+        }
+    },
+    
+    update: function(){
+        if(state.current !== state.game) return;
+        
+        if(frames%100 == 0){
+            this.position.push({
+                x : board.width,
+                y : this.maxYPos * ( Math.random() + 1)
+            });
+        }
+        for(let i = 0; i < this.position.length; i++){
+            let p = this.position[i];
+            
+            let bottomPipeYPos = p.y + this.h + this.gap;
+            
+            // COLLISION DETECTION
+            // TOP PIPE
+            if(zambird.x + zambird.radius > p.x && zambird.x - zambird.radius < p.x + this.w && zambird.y + zambird.radius > p.y && zambird.y - zambird.radius < p.y + this.h){
+                state.current = state.over;
+                HIT.play();
+            }
+            // BOTTOM PIPE
+            if(zambird.x + zambird.radius > p.x && zambird.x - zambird.radius < p.x + this.w && zambird.y + zambird.radius > bottomPipeYPos && zambird.y - zambird.radius < bottomPipeYPos + this.h){
+                state.current = state.over;
+                HIT.play();
+            }
+            
+            // MOVE THE PIPES TO THE LEFT
+            p.x -= this.dx;
+            
+            // if the pipes go beyond canvas, we delete them from the array
+            if(p.x + this.w <= 0){
+                this.position.shift();
+                score.value += 1;
+                SCORE_S.play();
+                score.best = Math.max(score.value, score.best);
+                localStorage.setItem("best", score.best);
+            }
+        }
+    },
+    
+    reset : function(){
+        this.position = [];
+    }
+    
+}
+
+// SCORE
+const score= {
+    best : parseInt(localStorage.getItem("best")) || 0,
+    value : 0,
+    
+    draw : function(){
+        boardContext.fillStyle = "#FFF";
+        boardContext.strokeStyle = "#000";
+        
+        if(state.current == state.game){
+            boardContext.lineWidth = 2;
+            boardContext.font = "35px Teko";
+            boardContext.fillText(this.value, board.width/2, 50);
+            boardContext.strokeText(this.value, board.width/2, 50);
+            
+        }else if(state.current == state.over){
+            // SCORE VALUE
+            boardContext.font = "25px Teko";
+            boardContext.fillText(this.value, 225, 186);
+            boardContext.strokeText(this.value, 225, 186);
+            // BEST SCORE
+            boardContext.fillText(this.best, 225, 228);
+            boardContext.strokeText(this.best, 225, 228);
+        }
+    },
+    
+    reset : function(){
+        this.value = 0;
+    }
+}
+
+// DRAW
+function draw(){
+    boardContext.fillStyle = "#70c5ce";
+    boardContext.fillRect(0, 0, board.width, board.height);
+    
+    background.draw();
+    pipes.draw();
+    foreground.draw();
+    zambird.draw();
+    getReady.draw();
+    gameOver.draw();
+    score.draw();
+}
+
+// UPDATE
+function update(){
+    zambird.update();
+    foreground.update();
+    pipes.update();
+}
+
+// LOOP
+function loop(){
+    update();
+    draw();
+    frames++;
+    
+    requestAnimationFrame(loop);
+}
+loop();
+
+
+
+
